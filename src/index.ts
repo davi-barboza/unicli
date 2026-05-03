41de4f4f189f2595591329d2b1851fe50e31efa1
Davi Barboza <67708096+davi-barboza@users.noreply.github.com>
2026-05-03 13:17:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name("unicli")
   .description("Universal CLI to generate files")
-  .version("1.0.0");
+  .version("1.0.14");
 
 // Configura os comandos
 setupGenerateCommand(program);
